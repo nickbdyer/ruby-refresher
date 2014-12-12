30e08fdf93cc19ed202198de0378825da855369c
Nick Dyer <nbdyer@gmail.com>
2014-12-12 15:34:20 +0000

    remove nil from array

--- lib/questions.rb
+++ lib/questions.rb
@@ -5,11 +5,12 @@ end
 
 # keep only the elements that start with a vowel
 def select_elements_starting_with_vowel(array)
-    array.select { |elem| ['a', 'e', 'i', 'o', 'u'].include?(elem[0]) }
+  array.select { |elem| ['a', 'e', 'i', 'o', 'u'].include?(elem[0]) }
 end
 
 # remove instances of nil (but NOT false) from an array
 def remove_nils_from_array(array)
+  array.compact
 end
 
 # remove instances of nil AND false from an array
