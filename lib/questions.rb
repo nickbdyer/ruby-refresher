86ab946e6e2402c1f901b9d4587632f8908cc000
Nick Dyer <nbdyer@gmail.com>
2014-12-13 10:50:38 +0000

    Convert array of words to letters refactor

--- lib/questions.rb
+++ lib/questions.rb
@@ -131,8 +131,10 @@ end
 # . e.g. the array ['cat', 'dog', 'fish'] becomes
 # ['a', 'c', 'd', 'f', 'g', 'h', 'i', 'o', 's', 't']
 def get_all_letters_in_array_of_words(array)
-  array.length.times{ array.shift.each_char { |c| array << c } }
-  array.sort
+  array.map! { |word| word.each_char.to_a }
+  array.flatten.sort
+  # array.length.times{ array.shift.each_char { |c| array << c } }
+  # array.sort
 end
 
 # swap the keys and values in a hash. e.g.
