97f48d7117e7b4a2a1c00c5a58cc06a2066c8d26
Nick Dyer <nbdyer@gmail.com>
2014-12-15 12:01:36 +0000

    Fizzbuzz without modulo

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -3,6 +3,34 @@
 # go from 1 to 100
 # (there's no RSpec test for this one)
 def fizzbuzz_without_modulo
-
-  
+  i = 0
+  while i < 100 
+    i += 1
+    puts fizzbuzz_says(i)
+  end
 end
+
+def fizzbuzz_says(number)
+  return "Fizzbuzz" if divisible_by_fifteen(number)
+  return "Buzz" if divisible_by_five(number)
+  return "Fizz" if divisible_by_three(number)
+  return number
+end
+
+def divisible_by_three(number)
+  is_divisible_by(number, 3)
+end
+
+def divisible_by_five(number)
+  is_divisible_by(number, 5)
+end
+
+def divisible_by_fifteen(number)
+  is_divisible_by(number, 15)
+end
+
+def is_divisible_by(number, divisor)
+  return (number / divisor.to_f) == (number / divisor)
+end
+
+fizzbuzz_without_modulo
