385b29c21c585b823d834448128a2c62a3fe5624
Nick Dyer <nbdyer@gmail.com>
2014-12-15 14:27:24 +0000

    Refactor fizzbuzz

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -3,18 +3,14 @@
 # go from 1 to 100
 # (there's no RSpec test for this one)
 def fizzbuzz_without_modulo
-  i = 0
-  while i < 100 
-    i += 1
-    puts fizzbuzz_says(i)
-  end
+  (1..100).each { |i| puts fizzbuzz_says(i)}
 end
 
 def fizzbuzz_says(number)
   return "Fizzbuzz" if divisible_by_fifteen(number)
   return "Buzz" if divisible_by_five(number)
   return "Fizz" if divisible_by_three(number)
-  return number
+  number
 end
 
 def divisible_by_three(number)
@@ -30,7 +26,7 @@ def divisible_by_fifteen(number)
 end
 
 def is_divisible_by(number, divisor)
-  return (number / divisor.to_f) == (number / divisor)
+  (number / divisor.to_f) == (number / divisor)
 end
 
 fizzbuzz_without_modulo
